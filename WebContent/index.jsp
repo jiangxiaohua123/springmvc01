e6e9ca202342e6c164c0264af9a0632035b26bb3
jiangxiaohua <363369419@qq.com>
2018-12-02 22:46:39 +0800

    过滤器  HiddenHttpMethodFilter的使用，请求方法的转换

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,5 +8,30 @@
 </head>
 <body>
    <a href="hello">test Hello</a>
+   <br/>
+   <!-- 查询id为1001的订单 -->
+   <a href="order/1001">REST GET</a>
+   <br/>
+   
+   <!-- 删除id为1001的订单 -->
+   <form action="order/1001" method="post">
+   		<input type="hidden" name="_method" value="DELETE"/>
+   		<input type="submit" value="REST DELETE"/>	
+   </form>
+   <br/>
+   
+   <!-- 添加一个新的订单 -->
+   <form action="order" method="post">
+   		<input type="submit" value="REST POST">
+   </form>
+   </br>
+   
+   <!-- 修改一个订单 -->
+   <form action="order" method="post">
+   		<input type="hidden" name="_method" value="PUT"/>
+   		<input type="submit" value="REST PUT" />
+   </form>
+   </br>
+   
 </body>
 </html>
--- src/com/demo/springmvc/helloWorld/HelloController.java
+++ src/com/demo/springmvc/helloWorld/HelloController.java
@@ -1,15 +1,53 @@
 package com.demo.springmvc.helloWorld;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
+
 	
 	@RequestMapping("/hello")
 	public String handleHelo() {
 		System.out.println("hello spring mvc");
 		return "success";
 	}
+	
+	/**
+	 * REST GET
+	 */
+	@RequestMapping(value="/order/{id}",method=RequestMethod.GET)
+	public String testRestGET(@PathVariable("id") String id) {
+		System.out.println("REST GET: "+ id);
+		return "success";
+	}
+	
+	/**
+	 * REST DELETE
+	 */
+	@RequestMapping(value="/order/{id}",method=RequestMethod.DELETE)
+	public String testRestDELETE(@PathVariable("id") String id) {
+		System.out.println("REST DELETE:  "+id);
+		return "success";
+	}
+	
+	/**
+	 * REST POST
+	 */
+	@RequestMapping(value="/order",method=RequestMethod.POST)
+	public String testPOST() {
+		System.out.println("REST POST");
+		return "success";
+	}
 
+	/**
+	 * REST PUT
+	 */
+	@RequestMapping(value="/order",method=RequestMethod.PUT)
+	public String testPUT() {
+		System.out.println("REST PUT");
+		return "success";
+	}
 }
